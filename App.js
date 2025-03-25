e7918a1a4d8f156794e6eac4068eac74fdc4aab0
WinyLeal <diegopatatita@gmail.com>
2025-03-25 23:22:02 +0100

    creamos la navegación

--- App.js
+++ App.js
@@ -1,22 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import "@/global.css";
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { createStaticNavigation } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HomeScreen } from './navigation/screens/Home';
 
-export default function App() {
-  return (
-    <GluestackUIProvider mode="light"><View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View></GluestackUIProvider>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const RootStack = createNativeStackNavigator({
+  screens: {
+    Home: HomeScreen,
   },
 });
+
+const Navigation = createStaticNavigation(RootStack);
+
+export default function App() {
+  return <Navigation options={{ headerShown: false }} />;
+}
