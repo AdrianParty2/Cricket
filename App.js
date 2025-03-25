7763d9007e09bfb91eb589520a04f8021b76d6b7
WinyLeal <diegopatatita@gmail.com>
2025-03-25 21:37:35 +0100

    .

--- App.js
+++ App.js
@@ -5,10 +5,12 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <GluestackUIProvider mode="light"><View style={styles.container}>
+    <GluestackUIProvider mode="light">
+      <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <StatusBar style="auto" />
-      </View></GluestackUIProvider>
+      </View>
+    </GluestackUIProvider>
   );
 }
 
